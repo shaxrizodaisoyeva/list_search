4e7911ec083fb413bda7f05f8c39f0bc7f0adc8e
shaxrizodaisoyeva <shaxrizodaisoyeva24@gmail.com>
2023-01-08 12:57:29 +0500

    4

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,8 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    a=len(data)
+    b=data.index(data[(a-1)])
+    return b
+data=[1,2,3,4,5,6,7,8,9,10,11,19,13]
+print(find_max_index(data))
